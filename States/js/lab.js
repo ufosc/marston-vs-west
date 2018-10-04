7d31fd7a0581725b16b452bebf46fb9d7ebe84a1
Alejandro Santacoloma <asantacoloma24@yahoo.com>
2018-10-03 23:27:13 -0400

    Create unique fighter files

---
+++ States/js/lab.js
@@ -0,0 +1,16 @@
+  class lab extends Fighter {
+      constructor(character,health,lives,startx,starty,controlnum) {
+  
+        super(character,health,lives,startx,starty,controlnum);
+        this.character.body.gravity.y = 650;
+        //console.log("we created the lab construtor");
+  
+          this.jumpSpeed = 25;
+          this.fallSpeed = 50;
+          this.runSpeed = 50;
+          this.attackSpeed = 1; //250;
+          this.attackDmg = 1;
+          this.moveSpeed = 200;
+  
+      }
+  }
