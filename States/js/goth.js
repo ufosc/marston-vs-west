f4189210eb74ce687993290a77015cefff11c8e7
Alejandro Santacoloma <asantacoloma24@yahoo.com>
2019-07-15 14:56:47 -0400

    New Fighter sprites in use

--- States/js/goth.js
+++ States/js/goth.js
@@ -17,26 +17,27 @@ class goth extends Fighter {
         //idle animation
         this.aniHang = this.character.animations.add('hang', [24, 25, 26], 5, true);
 
-        this.aniRight = this.character.animations.add('right', [3, 4, 5, 6, 7, 8, 9, 10, 11], 10, true);
+        this.aniRight = this.character.animations.add('right', [6, 7, 8, 9, 10, 11, 12], 10, true);
         this.aniRight.onComplete.add(this.walkEnd, this);
 
         //idle animation
-        this.aniIdle = this.character.animations.add('idle', [3, 4], 5, true);
+        this.aniIdle = this.character.animations.add('idle', [3, 4, 5], 5, true);
 
         //jump animation
         this.aniJump = this.character.animations.add('jump', [23, 23], 5, false); //need to adjust animation speed
         this.aniJump.onStart.add(this.jumpStart, this);
         this.aniJump.onComplete.add(this.jumpEnd, this);
+        
         //shield animation
         this.aniShield = this.character.animations.add('shield', [13], 5, false);
         this.aniShield.onComplete.add(this.shieldEnd, this);
 
         //punch animations
-        this.aniPunchWindUp= this.character.animations.add('punchwindup', [20, 20, 20], 5, false);
+        this.aniPunchWindUp= this.character.animations.add('punchwindup', [15, 16], 5, false);
         this.aniPunchWindUp.onStart.add(this.PunchWindUpStart, this);
         this.aniPunchWindUp.onComplete.add(this.PunchWindUpEnd, this);
 
-        this.aniPunch = this.character.animations.add('punch', [20, 21, 22], 10, false);
+        this.aniPunch = this.character.animations.add('punch', [20], 10, false);
         this.aniPunch.onStart.add(this.punchStart, this);
         this.aniPunch.onComplete.add(this.punchEnd, this);
 
@@ -55,16 +56,16 @@ class goth extends Fighter {
         this.aniDash.onComplete.add(this.dashEnd, this);
 
         //Tatsumaki (i am weeb)
-        this.aniJumpKickWindUp= this.character.animations.add('JumpKickwindup', [14,14], 5, false);
+        this.aniJumpKickWindUp= this.character.animations.add('JumpKickwindup', [14, 15, 16], 5, false);
         this.aniJumpKickWindUp.onStart.add(this.JumpKickWindUpStart, this);
         this.aniJumpKickWindUp.onComplete.add(this.JumpKickWindUpEnd, this);
 
-        this.aniJumpKick = this.character.animations.add('jumpKick', [14, 13, 12, 12, 12], 5, false);
+        this.aniJumpKick = this.character.animations.add('jumpKick', [17, 18, 19], 5, false);
         this.aniJumpKick.onStart.add(this.jumpKickStart, this);
         this.aniJumpKick.onComplete.add(this.jumpKickEnd, this);
 
         //Uppercut (change 16 later for a better uppercut frame)
-        this.aniUppercutWindUp= this.character.animations.add('Uppercutwindup', [14, 15, 16, 16, 16], 5, false);
+        this.aniUppercutWindUp= this.character.animations.add('Uppercutwindup', [14, 15, 16, 16], 5, false);
         this.aniUppercutWindUp.onStart.add(this.UppercutWindUpStart, this);
         this.aniUppercutWindUp.onComplete.add(this.UppercutWindUpEnd, this);
         
@@ -82,47 +83,47 @@ class goth extends Fighter {
         this.aniWarlock.onComplete.add(this.warlockEnd, this);
 
         //player got hit animation
-        this.aniKo = this.character.animations.add('ko', [23], 5, false);
+        this.aniKo = this.character.animations.add('ko', [27, 28], 5, false);
         this.aniKo.onComplete.add(this.koEnd, this);
 
         //Player air swipe forward/neutral
-        this.aniAirFWindUp= this.character.animations.add('AirFwindup', [24,24], 5, false);
+        this.aniAirFWindUp= this.character.animations.add('AirFwindup', [20], 5, false);
         this.aniAirFWindUp.onStart.add(this.AirFWindUpStart, this);
         this.aniAirFWindUp.onComplete.add(this.AirFWindUpEnd, this);
         
-        this.aniAirF = this.character.animations.add('airforward', [24, 24, 25, 25, 25], 7, false);
+        this.aniAirF = this.character.animations.add('airforward', [21, 22], 7, false);
         this.aniAirF.onStart.add(this.airforwardStart, this);
         this.aniAirF.onComplete.add(this.airforwardEnd, this);
 
         //Player air swipe down
-        this.aniAirDWindUp= this.character.animations.add('AirDwindup', [28,28], 5, false);
+        this.aniAirDWindUp= this.character.animations.add('AirDwindup', [20], 5, false);
         this.aniAirDWindUp.onStart.add(this.AirDWindUpStart, this);
         this.aniAirDWindUp.onComplete.add(this.AirDWindUpEnd, this);
 
-        this.aniAirD = this.character.animations.add('airdown', [28, 28, 29, 30], 7, false);
+        this.aniAirD = this.character.animations.add('airdown', [14], 7, false);
         this.aniAirD.onStart.add(this.airdownStart, this);
         this.aniAirD.onComplete.add(this.airdownEnd, this);
 
         //Player air swipe neutral
-        this.aniAirNWindUp= this.character.animations.add('airNwindup', [31,31], 5, false);
+        this.aniAirNWindUp= this.character.animations.add('airNwindup', [21], 5, false);
         this.aniAirNWindUp.onStart.add(this.AirNWindUpStart, this);
         this.aniAirNWindUp.onComplete.add(this.AirNWindUpEnd, this);
 
-        this.aniAirN = this.character.animations.add('airneutral', [31, 31, 32, 32], 7, false);
+        this.aniAirN = this.character.animations.add('airneutral', [22, 23], 7, false);
         this.aniAirN.onStart.add(this.airneutralStart, this);
         this.aniAirN.onComplete.add(this.airneutralEnd, this);
 
         //Player juggle
-        this.aniJuggleWindUp= this.character.animations.add('Jugglewindup', [26,26], 5, false);
+        this.aniJuggleWindUp= this.character.animations.add('Jugglewindup', [21], 5, false);
         this.aniJuggleWindUp.onStart.add(this.JuggleWindUpStart, this);
         this.aniJuggleWindUp.onComplete.add(this.JuggleWindUpEnd, this);
 
-        this.aniJuggle = this.character.animations.add('juggle', [26, 21, 22, 23], 7, false);
+        this.aniJuggle = this.character.animations.add('juggle', [22, 22], 7, false);
         this.aniJuggle.onStart.add(this.JuggleStart, this);
         this.aniJuggle.onComplete.add(this.JuggleEnd, this);
 
         //Player Air Dodge
-        this.aniAirDodge = this.character.animations.add('airDodge', [6], 15, false);
+        this.aniAirDodge = this.character.animations.add('airDodge', [20], 15, false);
         this.aniAirDodge.onStart.add(this.airDodgeStart, this);
         this.aniAirDodge.onComplete.add(this.airDodgeEnd, this);
 
--- States/js/lab.js
+++ States/js/lab.js
@@ -12,5 +12,120 @@ class lab extends Fighter {
         this.attackDmg = 1;
         this.moveSpeed = 200;
 
+        //Player animations
+
+        //idle animation
+        this.aniHang = this.character.animations.add('hang', [24, 25, 26], 5, true);
+
+        this.aniRight = this.character.animations.add('right', [6, 7, 8, 9], 10, true);
+        this.aniRight.onComplete.add(this.walkEnd, this);
+
+        //idle animation
+        this.aniIdle = this.character.animations.add('idle', [1, 2, 3, 4, 5], 5, true);
+
+        //jump animation
+        this.aniJump = this.character.animations.add('jump', [20], 5, false); //need to adjust animation speed
+        this.aniJump.onStart.add(this.jumpStart, this);
+        this.aniJump.onComplete.add(this.jumpEnd, this);
+        
+        //shield animation
+        this.aniShield = this.character.animations.add('shield', [20], 5, false);
+        this.aniShield.onComplete.add(this.shieldEnd, this);
+
+        //punch animations
+        this.aniPunchWindUp= this.character.animations.add('punchwindup', [19], 5, false);
+        this.aniPunchWindUp.onStart.add(this.PunchWindUpStart, this);
+        this.aniPunchWindUp.onComplete.add(this.PunchWindUpEnd, this);
+
+        this.aniPunch = this.character.animations.add('punch', [18], 10, false);
+        this.aniPunch.onStart.add(this.punchStart, this);
+        this.aniPunch.onComplete.add(this.punchEnd, this);
+
+        //kick
+        this.aniKickWindUp= this.character.animations.add('kickwindup', [20, 20, 20], 10, false);
+        this.aniKickWindUp.onStart.add(this.KickWindUpStart, this);
+        this.aniKickWindUp.onComplete.add(this.KickWindUpEnd, this);
+
+        this.aniKick = this.character.animations.add('kick', [21, 21, 22, 22, 22], 10, false);
+        this.aniKick.onStart.add(this.kickStart, this);
+        this.aniKick.onComplete.add(this.kickEnd, this);
+
+        //dash
+        this.aniDash = this.character.animations.add('dash', [6], 15, false);
+        this.aniDash.onStart.add(this.dashStart, this);
+        this.aniDash.onComplete.add(this.dashEnd, this);
+
+        //Tatsumaki (i am weeb)
+        this.aniJumpKickWindUp= this.character.animations.add('JumpKickwindup', [10, 11, 12, 13], 5, false);
+        this.aniJumpKickWindUp.onStart.add(this.JumpKickWindUpStart, this);
+        this.aniJumpKickWindUp.onComplete.add(this.JumpKickWindUpEnd, this);
+
+        this.aniJumpKick = this.character.animations.add('jumpKick', [14, 15], 5, false);
+        this.aniJumpKick.onStart.add(this.jumpKickStart, this);
+        this.aniJumpKick.onComplete.add(this.jumpKickEnd, this);
+
+        //Uppercut (change 16 later for a better uppercut frame)
+        this.aniUppercutWindUp= this.character.animations.add('Uppercutwindup', [21, 22], 5, false);
+        this.aniUppercutWindUp.onStart.add(this.UppercutWindUpStart, this);
+        this.aniUppercutWindUp.onComplete.add(this.UppercutWindUpEnd, this);
+        
+        this.aniUppercut = this.character.animations.add('uppercut', [23], 5, false);
+        this.aniUppercut.onStart.add(this.uppercutStart, this);
+        this.aniUppercut.onComplete.add(this.uppercutEnd, this);
+
+        //Slow warlock punch
+        this.aniWarlockWindUp= this.character.animations.add('Warlockwindup', [10, 11, 12, 13], 5, false);
+        this.aniWarlockWindUp.onStart.add(this.WarlockWindUpStart, this);
+        this.aniWarlockWindUp.onComplete.add(this.WarlockWindUpEnd, this);
+        
+        this.aniWarlock = this.character.animations.add('warlock', [14, 15], 7, false);
+        this.aniWarlock.onStart.add(this.warlockStart, this);
+        this.aniWarlock.onComplete.add(this.warlockEnd, this);
+
+        //player got hit animation
+        this.aniKo = this.character.animations.add('ko', [27, 28], 5, false);
+        this.aniKo.onComplete.add(this.koEnd, this);
+
+        //Player air swipe forward/neutral
+        this.aniAirFWindUp= this.character.animations.add('AirFwindup', [14], 5, false);
+        this.aniAirFWindUp.onStart.add(this.AirFWindUpStart, this);
+        this.aniAirFWindUp.onComplete.add(this.AirFWindUpEnd, this);
+        
+        this.aniAirF = this.character.animations.add('airforward', [15], 7, false);
+        this.aniAirF.onStart.add(this.airforwardStart, this);
+        this.aniAirF.onComplete.add(this.airforwardEnd, this);
+
+        //Player air swipe down
+        this.aniAirDWindUp= this.character.animations.add('AirDwindup', [20], 5, false);
+        this.aniAirDWindUp.onStart.add(this.AirDWindUpStart, this);
+        this.aniAirDWindUp.onComplete.add(this.AirDWindUpEnd, this);
+
+        this.aniAirD = this.character.animations.add('airdown', [8], 7, false);
+        this.aniAirD.onStart.add(this.airdownStart, this);
+        this.aniAirD.onComplete.add(this.airdownEnd, this);
+
+        //Player air swipe neutral
+        this.aniAirNWindUp= this.character.animations.add('airNwindup', [15], 5, false);
+        this.aniAirNWindUp.onStart.add(this.AirNWindUpStart, this);
+        this.aniAirNWindUp.onComplete.add(this.AirNWindUpEnd, this);
+
+        this.aniAirN = this.character.animations.add('airneutral', [14], 7, false);
+        this.aniAirN.onStart.add(this.airneutralStart, this);
+        this.aniAirN.onComplete.add(this.airneutralEnd, this);
+
+        //Player juggle
+        this.aniJuggleWindUp= this.character.animations.add('Jugglewindup', [15, 14], 5, false);
+        this.aniJuggleWindUp.onStart.add(this.JuggleWindUpStart, this);
+        this.aniJuggleWindUp.onComplete.add(this.JuggleWindUpEnd, this);
+
+        this.aniJuggle = this.character.animations.add('juggle', [13], 7, false);
+        this.aniJuggle.onStart.add(this.JuggleStart, this);
+        this.aniJuggle.onComplete.add(this.JuggleEnd, this);
+
+        //Player Air Dodge
+        this.aniAirDodge = this.character.animations.add('airDodge', [20], 15, false);
+        this.aniAirDodge.onStart.add(this.airDodgeStart, this);
+        this.aniAirDodge.onComplete.add(this.airDodgeEnd, this);
+
     }
 }
